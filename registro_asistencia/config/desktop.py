cb626c3f93c22d0e1a07a20a61af0eb3cfb68879
maxinisalo97 <maxinisalo97@gmail.com>
2024-08-29 11:26:30 +0000

    Cambios v5

--- registro_asistencia/config/desktop.py
+++ registro_asistencia/config/desktop.py
@@ -1,5 +1,3 @@
-# registro_asistencia/config/desktop.py
-
 from frappe import _
 
 def get_data():
@@ -9,43 +7,22 @@ def get_data():
             "color": "#FF5733",
             "icon": "octicon octicon-checklist",
             "type": "module",
-            "label": _("Registro Asistencia"),
+            "label": _("Registro de Asistencia"),
             "items": [
+                {
+                    "type": "doctype",
+                    "name": "App Settings",
+                    "label": _("Configuración de Registro de Asistencia"),
+                    "description": _("Configuración general de la aplicación de Registro de Asistencia."),
+                    "onboard": 1,
+                    "dependencies": ["App Settings"]
+                },
                 {
                     "type": "page",
                     "name": "registro_asistencia",
                     "label": _("Registro de Asistencia"),
-                    "description": _("Página para registrar asistencia"),
-                    "link": "/registro_asistencia",
-                    "icon": "octicon octicon-file",
-                },
-                {
-                    "type": "doctype",
-                    "name": "App Settings",
-                    "label": _("Configuración de Registro de Asistencia"),
-                    "description": _("Configuración de parámetros para Registro de Asistencia"),
-                    "onboard": 1,
-                    "icon": "octicon octicon-gear",
-                    "link": "Form/App Settings/App Settings",
-                    "doctype": "App Settings"
-                }
-            ]
-        },
-        {
-            "module_name": "Configuración de ERP",
-            "color": "#FF5733",
-            "icon": "octicon octicon-gear",
-            "type": "module",
-            "label": _("Configuración de ERP"),
-            "items": [
-                {
-                    "type": "doctype",
-                    "name": "App Settings",
-                    "label": _("Configuración de Registro de Asistencia"),
-                    "description": _("Configura los parámetros de la aplicación de Registro de Asistencia"),
-                    "icon": "octicon octicon-gear",
-                    "link": "Form/App Settings/App Settings",
-                    "doctype": "App Settings"
+                    "description": _("Página principal de Registro de Asistencia."),
+                    "icon": "fa fa-check-square"
                 }
             ]
         }
